f3a66ffc25aec57364af18f7d76d1fd903b3a296
billythekidd <labaybillyjames@gmail.com>
2026-03-04 01:36:15 +0800

    chore: project init

---
+++ src/main/java/io/github/thebrodigy/awezomlogger/AwezomLoggerApplication.java
@@ -0,0 +1,13 @@
+package io.github.thebrodigy.awezomlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwezomLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwezomLoggerApplication.class, args);
+    }
+
+}
